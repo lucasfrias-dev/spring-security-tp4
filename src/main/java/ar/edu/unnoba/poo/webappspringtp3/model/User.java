29256c311ae88dbc94f93036b1ff82cf79ad5c63
lucasfrias-dev <lucas.frias.ing@gmail.com>
2023-10-12 12:30:01 -0300

    Create User.java

---
+++ src/main/java/ar/edu/unnoba/poo/webappspringtp3/model/User.java
@@ -0,0 +1,24 @@
+package ar.edu.unnoba.poo.webappspringtp3.model;
+
+public class User {
+    private String email;
+    private String password;
+
+    // Getters y setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
