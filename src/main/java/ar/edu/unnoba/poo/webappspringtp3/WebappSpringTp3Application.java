340bdf62e5d4b8b917a3d7086cb0d7684a932f82
lucasfrias-dev <lucas.frias.ing@gmail.com>
2023-10-12 12:19:25 -0300

    commit inicial
    Estructura y archivos iniciales

---
+++ src/main/java/ar/edu/unnoba/poo/webappspringtp3/WebappSpringTp3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo.webappspringtp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappSpringTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappSpringTp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/poo/webappspringtp3/WebappSpringTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo.webappspringtp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappSpringTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
