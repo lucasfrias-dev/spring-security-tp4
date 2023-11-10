dbff6fe418ed96e589200c6d0720b2d7a5c63e97
lucasfrias-dev <lucas.frias.ing@gmail.com>
2023-11-10 19:33:09 -0300

    Update WebappSpringApplication.java
    Se cambio el nombre para que sea mas general

---
+++ src/main/java/ar/edu/unnoba/poo/webappspring/WebappSpringApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo.webappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappSpringApplication.class, args);
+	}
+
+}
